6474ccd5f5d0a7914628d00e8f7194d06e1adf70
Synapt1x <chriscadonic@gmail.com>
2018-11-07 16:40:10 -0600

    Add some more work; add way to remove points going wrong direction

--- mwmtracker/tracker.py
+++ mwmtracker/tracker.py
@@ -498,18 +498,28 @@ class Tracker:
 
         x_coord = self.data['x'][-1]
         y_coord = self.data['y'][-1]
+        prev_2_x_coord = self.data['x'][-2]
+        prev_2_y_coord = self.data['y'][-2]
         if self.config['boundPool']:
             x_coord -= self.config['minx']
             y_coord -= self.config['miny']
+            prev_2_x_coord -= self.config['minx']
+            prev_2_y_coord -= self.config['miny']
 
         ask_frame = cv2.circle(ask_frame, (x_coord, y_coord),
                            thickness=5, color=(0, 255, 0), radius=2)
+        ask_frame = cv2.circle(ask_frame, (prev_2_x_coord, prev_2_y_coord),
+                               thickness=2, color=(0, 255, 0), radius=1)
 
         while True:
             cv2.imshow("Cannot detect. Please click mouse location.", ask_frame)
             cv2.setMouseCallback("Cannot detect. Please click mouse location.",
                                  fail_detect_click)
             key = cv2.waitKey(1) & 0xFF
+
+            if key == ord("z"):
+                self.data['x'][-1] = self.data['x'][-2]
+                self.data['y'][-1] = self.data['y'][-2]
 
             # if the 'c' key or space bar is pressed, break from the loop
             if key == ord("c") or key == 32:
